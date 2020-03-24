0bf7a48258520d9c57f678fe448ced54e101e9cc
LukasSchierle <wi18289@lehre.dhbw-stuttgart.de>
2020-03-24 15:14:07 +0100

    added login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DozentenlisteComponent } from './verwaltung/dozentenliste/dozentenliste.component';
 import { NeudozentComponent } from './verwaltung/neudozent/neudozent.component';
+import { LoginComponent } from './einstieg/login/login.component';
 
 
 const routes: Routes = [
   {path: 'dozentenliste', component: DozentenlisteComponent},
-  {path: 'dozentenliste/neu', component: NeudozentComponent}
+  {path: 'dozentenliste/neu', component: NeudozentComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
