084be42ecbbcdf0e150d9b2750d1bbabdd5912f1
Alexander Elster <48534216+AlexanderElster@users.noreply.github.com>
2020-05-07 00:59:11 +0200

    überflüssige imports entfernt

--- backend/src/main/java/com/dhbw/verteiltesysteme/backend/repositories/KursRepositories.java
+++ backend/src/main/java/com/dhbw/verteiltesysteme/backend/repositories/KursRepositories.java
@@ -3,7 +3,6 @@ package com.dhbw.verteiltesysteme.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dhbw.verteiltesysteme.backend.entities.Kurs;
-import com.dhbw.verteiltesysteme.backend.entities.User;
 
 public interface KursRepositories extends JpaRepository<Kurs, Integer> {
 
